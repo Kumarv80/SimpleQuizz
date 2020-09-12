5764ce51e1cc90090b283a6fe877cf5657ae8792
Kumar V <kumarv@srmtchn.co.in>
2020-09-12 20:35:34 +0530

    Updates

---
+++ precache-manifest.8f0f5ed9665429b7d5b39a1ece5be5df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d787db89ac457946d3a8362a6884b0",
+    "url": "/simpleQuizz/index.html"
+  },
+  {
+    "revision": "5c6814767ee0b388124d",
+    "url": "/simpleQuizz/static/css/main.58bc9473.chunk.css"
+  },
+  {
+    "revision": "1e8fc0c4b30fbe8927d9",
+    "url": "/simpleQuizz/static/js/2.dc7f7dc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simpleQuizz/static/js/2.dc7f7dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c6814767ee0b388124d",
+    "url": "/simpleQuizz/static/js/main.655ef9d7.chunk.js"
+  },
+  {
+    "revision": "17f98879bcbb157a7923",
+    "url": "/simpleQuizz/static/js/runtime-main.0fbdb69b.js"
+  }
+]);
